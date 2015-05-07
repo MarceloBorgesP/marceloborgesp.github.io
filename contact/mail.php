b07aadc77a146eea340cbc35cf1350527dfb6351
MarceloBorgesP <borgespereira.marcelo@gmail.com>
2015-05-07 11:03:58 -0300

    Adding Pages

--- contact/mail.php
+++ contact/mail.php
@@ -1,8 +1,38 @@
 <html>
 <body>
 
-Welcome <?php echo $_POST["name"]; ?><br>
-Your email address is: <?php echo $_POST["email"]; ?>
+<?php 
+
+server {
+    listen 80;
+ 
+    server_name marceloborgesp.github.io/;
+ 
+    location / {
+        root /Portfolio/;
+        index index.html;
+ 
+        # this is the magic
+        error_page 405 =200 $uri;
+    }
+ 
+}
+//if "email" variable is filled out, send email
+  if (isset($_REQUEST['email']))  {
+  
+  //Email information
+  $admin_email = "borgespereira.marcelo@gmail.com";
+  $email = $_REQUEST['email'];
+  $subject = $_REQUEST['subject'];
+  $comment = $_REQUEST['comment'];
+  
+  //send email
+  mail($admin_email, "$subject", $comment, "From:" . $email);
+  
+  //Email response
+  echo "Thank you for contacting us!";
+  }
+?>
 
 </body>
 </html>
