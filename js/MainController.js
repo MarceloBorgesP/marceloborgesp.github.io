15f50cbd8a672fd8f8a7fc53b670a21743d0815b
MarceloBorgesP <borgespereira.marcelo@gmail.com>
2015-05-12 09:54:48 -0300

    Adding Pages

--- js/MainController.js
+++ js/MainController.js
@@ -1,29 +1,29 @@
 app.controller('MainController', ['$scope', function($scope) { 
   $scope.projects=[
       {
-        url:'projects/exchange/index.html',
+        url:'Portfolio/projects/exchange/index.html',
         image:'Portfolio/images/exchange-preview.png',
         title: 'Pelo mundo',
          description:'Its a project with many pictures taken during my exchange, I have made use of Carousel (Bootsrap) on it.'
          },
 
         {
-        url:'projects/google-map-voice-search/my-element.html',
-        image:'images/voice-search.png', 
+        url:'Portfolio/projects/google-map-voice-search/my-element.html',
+        image:'Portfolio/images/voice-search.png', 
         title: 'Google Map Voice Search', 
         description:'Its my first work with Web Components. When declared, this component can search for places on the web using voice detection'
         },
 
         {
-        url:'projects/contact/contact.html',
-        image:'images/contact.png', 
+        url:'Portfolio/projects/contact/contact.html',
+        image:'Portfolio/images/contact.png', 
         title: 'Contact me Form', 
         description:'Its the project of a form using webcomponents'
         },
 
         {
-        url:'projects/GuestList/index.html',
-        image:'images/guest-list.png', 
+        url:'Portfolio/projects/GuestList/index.html',
+        image:'Portfolio/images/guest-list.png', 
         title: 'Guest List', 
         description:'A guest list using local storage and AngularJS'
         }
