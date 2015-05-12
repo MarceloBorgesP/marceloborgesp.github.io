b4cae2a4a2b028d0fc2b8e86d45632fcf4b7302c
MarceloBorgesP <borgespereira.marcelo@gmail.com>
2015-05-12 09:53:08 -0300

    Adding Pages

--- js/MainController.js
+++ js/MainController.js
@@ -2,7 +2,7 @@ app.controller('MainController', ['$scope', function($scope) {
   $scope.projects=[
       {
         url:'projects/exchange/index.html',
-        image:'images/exchange-preview.png',
+        image:'Portfolio/images/exchange-preview.png',
         title: 'Pelo mundo',
          description:'Its a project with many pictures taken during my exchange, I have made use of Carousel (Bootsrap) on it.'
          },
