2403093903b7c785afbd12cd9f878bdb4dd3f211
MarceloBorgesP <borgespereira.marcelo@gmail.com>
2015-05-26 19:52:37 -0300

    Adding pages

---
+++ projects/Octo/js/index.js
@@ -0,0 +1,23 @@
+var main = function () {
+  	$("header").load("header.html");
+  	$("footer").load("footer.html");
+
+  	$('.bxslider').bxSlider({
+  	  pause: 10000,
+	  auto: true,
+	  autoControls: true
+	});
+
+	function initialize() {
+        var mapCanvas = document.getElementById('map-canvas');
+        var mapOptions = {
+          center: new google.maps.LatLng(44.5403, -78.5463),
+          zoom: 8,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
+        var map = new google.maps.Map(mapCanvas, mapOptions)
+      }
+      google.maps.event.addDomListener(window, 'load', initialize);
+}
+$(document).ready(main);
+
