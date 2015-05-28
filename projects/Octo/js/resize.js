75d087e3d662ce6ee8e3f402599d958f718b9cbc
MarceloBorgesP <borgespereira.marcelo@gmail.com>
2015-05-28 18:36:07 -0300

    Adding pages

--- projects/Octo/js/resize.js
+++ projects/Octo/js/resize.js
@@ -4,6 +4,14 @@ var resize = function () {
   var windowsize = $(window).width();
   var header = false;
 
+  $('figure').css( "width" , (90+windowsize)/5 );
+  $('figure').css( "height" , (90+windowsize)/6.25 );
+  $('.span-hover').css( "width" , (90+windowsize)/5 );
+  $('.span-hover').css( "height" , (90+windowsize)/6.25 );
+  $('.span-hover').css( "top" , -((90+windowsize)/6.25) );
+  $('img').css( "width" , (90+windowsize)/5 );
+  $('img').css( "height" , (90+windowsize)/6.25 );
+
   if(windowsize < 1050) {
 
         $('#menu-hamburger').css( "display" , "inherit" );
@@ -24,9 +32,15 @@ var resize = function () {
             $('#menu').css( "display" , "none" );
             $('header').css( "height" , "0" );
             header = false;
-          }
-          
+          }     
     	     });
+
+        $( '.close-menu' ).click(function(){
+            $('#menu').css( "display" , "none" );
+            $('.li-header').css( "display" , "none" );
+            $('header').css( "height" , "0" );
+            header = false;
+          });
   }
 
   else if (windowsize >= 1050) {
