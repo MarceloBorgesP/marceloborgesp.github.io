1570919c43123204d5d1b2eb6fa282076329e5b5
MarceloBorgesP <borgespereira.marcelo@gmail.com>
2015-05-27 23:42:19 -0300

    Adding pages

--- projects/Octo/js/index.js
+++ projects/Octo/js/index.js
@@ -11,8 +11,8 @@ var main = function () {
 	function initialize() {
         var mapCanvas = document.getElementById('map-canvas');
         var mapOptions = {
-          center: new google.maps.LatLng(44.5403, -78.5463),
-          zoom: 8,
+          center: new google.maps.LatLng(-22.223038, -45.921424),
+          zoom: 17,
           mapTypeId: google.maps.MapTypeId.ROADMAP
         }
         var map = new google.maps.Map(mapCanvas, mapOptions)
