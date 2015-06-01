4a23d18413dbff9950ecef627c3a735616b69cc6
MarceloBorgesP <borgespereira.marcelo@gmail.com>
2015-06-01 16:28:51 -0300

    Adding pages

--- projects/Octo/js/header.js
+++ projects/Octo/js/header.js
@@ -1,5 +1,5 @@
-
 var count = 0;    
+
     $("#arrow").click(function() {
 	if(count==0) {
 		$("#li-quem-somos").append("<ul class='quem-somos animated fadeIn'><li><a href='quem-somos.html#missao'>Missão</a></li><li><a href='quem-somos.html#visao'>Visão</a></li><li><a href='quem-somos.html#valores'>Valores</a></li> </ul>");
@@ -15,10 +15,14 @@ var count = 0;
 });
 
 $("#a-contato").click(function () {
+    var windowsize = $(window).width();
+
+    if(windowsize < 1050) {
         $('#menu').css( "display" , "none" );
         $('header').css( "height" , "0" );
         $('button').addClass('fa-bars');
         $('button').removeClass('fa-times');
         header = false;
+    }
 });
 
